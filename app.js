7a473ee71d2ad12b6a32813c4c16b12532b6265c
JongoSimon <simonjongo@gmail.com>
2025-08-24 19:12:12 -0500

    push

---
+++ app.js
@@ -0,0 +1,4 @@
+const carMakes = ['Toyota', 'Honda', 'Ford', 'BMW', 'Mercedes', 'Audi', 'Volkswagen', 'Nissan', 'Hyundai', 'Kia'];
+
+// Example: log all car makes
+console.log('Available car makes:', carMakes);
